9996fd7f0025f57c9c4f0b38b6c3c6828f44d060
Eleanor <elmcmurtry1@gmail.com>
2016-08-19 14:59:52 +1000

    Adding physics

--- Game1/Systems/BallControl.cs
+++ Game1/Systems/BallControl.cs
@@ -7,11 +7,83 @@ using System.Threading.Tasks;
 namespace ECS.Systems {
     class BallControl : System {
         public override void Update(Entity[] entities, float dt) {
+            // handle physics
+            foreach (Entity entity in entities) {
+                // Apply velocity
+                Components.Position posComp = entity.GetComponent<Components.Position>();
+                Components.Velocity velComp = entity.GetComponent<Components.Velocity>();
+                Components.Collidable collider = entity.GetComponent<Components.Collidable>();
+
+                if (posComp != null && velComp != null && collider != null) {
+                    // bounce the ball off the sides
+                    if (posComp.pos.X < 0 || posComp.pos.X > Game1.Width) {
+                        velComp.vel.X *= -1;
+                        if (posComp.pos.X < 0) {
+                            posComp.pos.X *= -1;
+                        } else {
+                            posComp.pos.X -= 2 * (posComp.pos.X - Game1.Width);
+                        }
+                    }
+                    if (posComp.pos.Y < 0 || posComp.pos.Y > Game1.Height) {
+                        velComp.vel.Y *= -1;
+                        if (posComp.pos.Y < 0) {
+                            posComp.pos.Y *= -1;
+                        } else {
+                            posComp.pos.Y -= 2 * (posComp.pos.Y - Game1.Height);
+                        }
+                    }
+                    posComp.pos += velComp.vel * dt;
+
+                    collider.collider.Pos = posComp.pos;
+                }
+            }
+            // handle collisions
             foreach (Entity entity in entities) {
                 Components.Position posComp = entity.GetComponent<Components.Position>();
                 Components.Velocity velComp = entity.GetComponent<Components.Velocity>();
-                if (posComp != null && velComp != null) {
-                    posComp.pos += velComp.vel * dt;
+                Components.Collidable collider = entity.GetComponent<Components.Collidable>();
+
+                if (posComp != null && velComp != null && collider != null) {
+                    foreach (Entity other in entities) {
+                        Components.Collidable otherCollider = other.GetComponent<Components.Collidable>();
+                        if (other != entity && otherCollider != null
+                            && collider.collider.Intersects(otherCollider.collider)) {
+                            // calculate impulse
+                            float v_x = velComp.vel.X;
+                            float v_y = velComp.vel.Y;
+                            float ov_x = 0;
+                            float ov_y = 0;
+                            // account for other impulse if the other object has velocity
+                            Components.Position otherPosComp = other.GetComponent<Components.Position>();
+                            Components.Velocity otherVelComp = other.GetComponent<Components.Velocity>();
+                            if (otherVelComp != null) {
+                                ov_x = otherVelComp.vel.X;
+                                ov_y = otherVelComp.vel.Y;
+                            }
+                            // calculate and apply impulse
+                            velComp.vel.X = (v_x * (collider.mass - otherCollider.mass)
+                                             + 2 * otherCollider.mass * ov_x)
+                                             / (collider.mass + otherCollider.mass);
+                            velComp.vel.Y = (v_y * (collider.mass - otherCollider.mass)
+                                             + 2 * otherCollider.mass * ov_y)
+                                             / (collider.mass + otherCollider.mass);
+                            posComp.pos.X += velComp.vel.X;
+                            posComp.pos.Y += velComp.vel.Y;
+                            // apply impulse to other object
+                            if (otherVelComp != null) {
+                                otherVelComp.vel.X = (ov_x * (otherCollider.mass - collider.mass)
+                                                      + 2 * collider.mass * v_x)
+                                                      / (collider.mass + otherCollider.mass);
+                                otherVelComp.vel.Y = (ov_y * (otherCollider.mass - collider.mass)
+                                                      + 2 * collider.mass * v_y)
+                                                      / (collider.mass + otherCollider.mass);
+                                if (otherPosComp != null) {
+                                    otherPosComp.pos.X += velComp.vel.X;
+                                    otherPosComp.pos.Y += velComp.vel.Y;
+                                }
+                            }                            
+                        }
+                    }
                 }
             }
         }
--- Game1/Systems/Renderer.cs
+++ Game1/Systems/Renderer.cs
@@ -8,20 +8,29 @@ using Microsoft.Xna.Framework.Graphics;
 
 namespace ECS.Systems {
     class Renderer : System {
-        
+        public const float ScaleFactor = 0.2f;
+
         public override void Render(Entity[] entities, SpriteBatch spriteBatch, float dt) {
             foreach (Entity entity in entities) {
                 Components.Position pos = entity.GetComponent<Components.Position>();
                 Components.Velocity vel = entity.GetComponent<Components.Velocity>();
                 Components.BallSprite bs = entity.GetComponent<Components.BallSprite>();
-
-
+                
                 if (pos != null && bs != null) {
                     Vector2 loc = pos.pos;
                     if (vel != null) {
                         loc += vel.vel / 2 * dt;
                     }
-                    spriteBatch.Draw(GetTexture("ball"), loc, Color.White);
+
+                    float scale = 1f;
+                    Components.Collidable collider = entity.GetComponent<Components.Collidable>();
+                    if (collider != null) {
+                        scale = ScaleFactor * collider.mass;
+                    }
+
+                    spriteBatch.Draw(GetTexture("ball"), loc, null,
+                                     Color.White, 0f, new Vector2(24, 24), scale,
+                                     SpriteEffects.None, 0);
                 }
             }
         }
